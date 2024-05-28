bde443c9dc8baaf9e69e43632426e5d62eb48bcd
calstu99 <andre_wal99@yahoo.com>
2024-05-27 21:41:53 -0700

    disable linting

--- next.config.mjs
+++ next.config.mjs
@@ -25,8 +25,13 @@ const nextConfig = {
 
 
       ],
+      
     },
-    
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 
 };
 
